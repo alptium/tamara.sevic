1c940d5bf6ea19f55c5bead2b3b455ede5b8265d
Tamaralb <35016727+Tamaralb@users.noreply.github.com>
2018-02-28 17:02:19 +0100

    elite-sports-team

--- elite-sports-team-selector/src/waterpoolteam/Main.java
+++ elite-sports-team-selector/src/waterpoolteam/Main.java
@@ -4,128 +4,103 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	static ArrayList<Pass> listPass = new ArrayList<>();
+	
 
-	@SuppressWarnings("null")
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Waterpool team!");
 		System.out.println("We're looking for new players.");
 		System.out.println("Please enter your details.");
 		System.out.println("---------------------------------------------------------");
-		try (Scanner sc = new Scanner(System.in);) {
+		ArrayList<Player> listPass = new ArrayList<>();
+		try (Scanner sc = new Scanner(System.in)) {
 
 			while (true) {
-				Player players = new Player();
+				Player player = new Player();
 
-				players = new Player();
+				player = new Player();
 				System.out.println("Enter your first name");
-				players.firstName = sc.next();
+				player.firstName = sc.next();
 				System.out.println("Enter your last name");
-				players.lastName = sc.next();
+				player.lastName = sc.next();
 				System.out.println("Sex (M/F)?");
-				players.sex = sc.next();
+				player.sex = sc.next();
 				System.out.println("Enter your age");
-				players.age = sc.nextInt();
+				player.age = sc.nextInt();
 				System.out.println("Enter your highschool");
-				players.Highschool = sc.next();
+				player.Highschool = sc.next();
 				System.out.println("Enter your Body height");
-				players.bodyheight = sc.nextDouble();
+				player.bodyheight = sc.nextDouble();
 				System.out.println("Enter your Body Macs Index");
-				players.BMI = sc.nextDouble();
+				player.BMI = sc.nextDouble();
 				System.out.println("Enter your time of swimming at 20m");
-				players.SWS20m = sc.nextDouble();
+				player.SWS20m = sc.nextDouble();
 				System.out.println("Enter your shoot speed");
-				players.SHOOT = sc.nextDouble();
+				player.SHOOT = sc.nextDouble();
 				
-				if(players.sex.equalsIgnoreCase("M")) {
-				if (players.bodyheight < 175) {
-					System.out.println("Sorry, but you are rejected");
-				} else if (players.BMI > 35) {
-					System.out.println("Sorry, but you are rejected");
-				} else if (players.SWS20m > 12) {
-					System.out.println("Sorry, but you are rejected");
-				} else if (players.SHOOT < 65) {
+				if(player.sex.equalsIgnoreCase("M")) {
+				if (player.bodyheight < 175 || player.BMI > 35 || player.SWS20m > 12 || player.SHOOT < 65) {
 					System.out.println("Sorry, but you are rejected");
 				} else {
 					System.out.println("Congratulations you are accepted");
-					Pass p = new Pass();
 
-					p.firstName = players.firstName;
-					p.lastName = players.lastName;
-					p.sex = players.sex;
-					p.age = players.age;
-					p.Highschool = players.Highschool;
-					p.bodyheight = players.bodyheight;
-					p.BMI = players.BMI;
-					p.SWS20m = players.SWS20m;
-					p.SHOOT = players.SHOOT;
+					listPass.add(player);
 
-					listPass.add(p);
-
-				}
+					}
 				}
 				
-					if(players.sex.equalsIgnoreCase("F")) {
-					if (players.bodyheight < 165) {
-						System.out.println("Sorry, but you are rejected");
-					} else if (players.BMI > 30) {
-						System.out.println("Sorry, but you are rejected");
-					} else if (players.SWS20m > 17) {
-						System.out.println("Sorry, but you are rejected");
-					} else if (players.SHOOT < 55) {
-						System.out.println("Sorry, but you are rejected");
-					} else {
-						System.out.println("Congratulations you are accepted");
-						Pass p = new Pass();
+				if(player.sex.equalsIgnoreCase("F")) {
+				if (player.bodyheight < 165 || player.BMI > 30 || player.SWS20m > 17 || player.SHOOT < 55) {
+					System.out.println("Sorry, but you are rejected");
+				} else {
+					System.out.println("Congratulations you are accepted");
 
-						p.firstName = players.firstName;
-						p.lastName = players.lastName;
-						p.sex = players.sex;
-						p.age = players.age;
-						p.Highschool = players.Highschool;
-						p.bodyheight = players.bodyheight;
-						p.BMI = players.BMI;
-						p.SWS20m = players.SWS20m;
-						p.SHOOT = players.SHOOT;
-
-						listPass.add(p);
-
+					listPass.add(player);
 					}
-					}
-
-				System.out.println("Are there new candidates, yes or no?");
-				String qu = sc.next();
-				if (qu.equalsIgnoreCase("no")) {
+				}
+					System.out.println("Are there new candidates, yes or no?");
+					String qu = sc.next();
+					if (qu.equalsIgnoreCase("no")) {
 					break;
 				}
+				else while (!qu.equalsIgnoreCase("yes") && !qu.equalsIgnoreCase("no"))  {
+					System.out.println("Sorry, but you are entered the wrong answer");
+					System.out.println("Are there new candidates, yes or no?");
+					qu = sc.next();
+				}
+					if (qu.equalsIgnoreCase("no")) {
+						break;
+					}
 			}
-			System.out.println("Candidates who have passed:");
-			int count = 1;
+			
+					System.out.println("Candidates who have passed:");
+			
+				int count = 1;
 
-			for (Pass p : listPass) {
+				for (Player player : listPass) {
 
-				System.out.println("");
-				System.out.println("Candidate " + count);
-				System.out.print("First name: " + p.firstName);
-				System.out.print(" Last name: " + p.lastName);
-				System.out.println(" Sex: " + p.sex);
-				System.out.print(" Age: " + p.age);
-				System.out.print(" Highschool: " + p.Highschool);
-				System.out.print(" Body height: " + p.bodyheight);
-				System.out.print(" BMI: " + p.BMI);
-				System.out.print(" SWS20m: " + p.SWS20m);
-				System.out.print(" Shoot: " + p.SHOOT);
-				System.out.println("");
-				System.out.println("---------------------------------------------------------");
-				count++;
+					System.out.println("");
+					System.out.println("Candidate " + count);
+					System.out.print("First name: " + player.firstName);
+					System.out.print(" Last name: " + player.lastName);
+					System.out.println(" Sex: " + player.sex);
+					System.out.print(" Age: " + player.age);
+					System.out.print(" Highschool: " + player.Highschool);
+					System.out.print(" Body height: " + player.bodyheight);
+					System.out.print(" BMI: " + player.BMI);
+					System.out.print(" SWS20m: " + player.SWS20m);
+					System.out.print(" Shoot: " + player.SHOOT);
+					System.out.println("");
+					System.out.println("---------------------------------------------------------");
+					count++;
+
+					}
+
+				} catch (Exception e) {
+					System.out.println("GRESKA!!!!!!!!!!!!!!");
+					System.out.println(e.toString());
+				}
 
 			}
 
-		} catch (Exception e) {
-			System.out.println("GRESKA!!!!!!!!!!!!!!");
-			System.out.println(e.toString());
 		}
-
-	}
-
-}
