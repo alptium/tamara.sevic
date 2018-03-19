d247eb8399e1504ffb02a1e2acfd80b24268f4fa
Tamaralb <35016727+Tamaralb@users.noreply.github.com>
2018-03-19 10:04:02 +0100

    Upload

---
+++ advanced-gradebook/src/advancedgradebook/Main.java
@@ -0,0 +1,59 @@
+package advancedgradebook;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		double totalGrade = 0;
+		double avgGrade = 0;
+		
+		try(Scanner sc = new Scanner(System.in)) {
+			
+			System.out.println("Please enter the number of students in your class: ");
+			
+			int totalNumberStudents = sc.nextInt();
+			
+			while (totalNumberStudents <= 0) {
+				System.out.println("Sorry, but you are entered the wrong answer");
+				System.out.println("The number of students must be 1 or greater.");
+				System.out.println("Please enter the number of students in your class: ");
+				totalNumberStudents = sc.nextInt();
+				}
+			
+			String[] firstName = new String[totalNumberStudents];
+			String[] lastName = new String[totalNumberStudents];
+			int[] grade = new int[totalNumberStudents];
+			
+			
+			for(int i = 0 ; i < totalNumberStudents; i++){ 
+				
+				int j = i + 1;
+				System.out.println("Enter the first name of your " + j + ". student: ");
+				firstName[i] = sc.next();
+				
+				System.out.println("Enter the last name of your " + j + ". student: ");
+				lastName[i] = sc.next();
+				
+				System.out.println("Enter the grade of your " + j + ". student: ");
+				grade[i]= sc.nextInt();
+			}
+	
+		        
+		    for(int i = 0; i < totalNumberStudents; i++) {
+		        	
+		    	totalGrade = totalGrade + grade[i];
+		    }
+		        
+		    System.out.println("Number of all students is " + totalNumberStudents);
+		    System.out.println("Total grade is: " + totalGrade); 
+		    
+		    avgGrade = totalGrade / totalNumberStudents;
+			
+			System.out.println("Average grade of all students is " + String.format("%.2g%n", avgGrade));
+		}
+
+	}
+
+}
